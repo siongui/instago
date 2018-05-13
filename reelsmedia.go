6bfcfff26dbde5fae27cec5d729b0d3847f54561
siongui <siongui@gmail.com>
2018-05-13 21:12:08 +0800

    change code to adapt to change of the format of reels_media story highlight tray

--- reelsmedia.go
+++ reelsmedia.go
@@ -3,12 +3,16 @@ package instago
 import (
 	"encoding/json"
 	"errors"
+	"strings"
 )
 
 const urlReelsMedia = `https://i.instagram.com/api/v1/feed/reels_media/`
 
 type highlightMedia struct {
-	Trays []IGStoryHighlightsTray `json:"reels_media"`
+	Reels struct {
+		ReelsMedia IGStoryHighlightsTray `json:"reels_media"`
+	} `json:"reels"`
+	Status string `json:"status"`
 }
 
 func (m *IGApiManager) GetHighlightsReelsMedia(id string) (tray IGStoryHighlightsTray, err error) {
@@ -18,15 +22,23 @@ func (m *IGApiManager) GetHighlightsReelsMedia(id string) (tray IGStoryHighlight
 		return
 	}
 
+	// The name of json field is the id of the highlight tray, which is only
+	// known in run-time, not compile-time. So we need to replace the id of
+	// the highlight tray with *reels_media*, which can be decoded by Go
+	// standard encoding/json package.
+	bb := []byte(strings.Replace(string(b), id, "reels_media", 1))
+
 	h := highlightMedia{}
-	err = json.Unmarshal(b, &h)
+	err = json.Unmarshal(bb, &h)
 	if err != nil {
 		return
 	}
-	if len(h.Trays) == 0 {
-		err = errors.New("reels_media has no highlight tray!")
+
+	// Check validity
+	if h.Reels.ReelsMedia.Id != id {
+		err = errors.New("Returned highlight tray seems weird")
 		return
 	}
-	tray = h.Trays[0]
+	tray = h.Reels.ReelsMedia
 	return
 }
