163990356a597af17b30cf543a62227dddbe287c
siongui <siongui@gmail.com>
2018-05-13 09:23:14 +0800

    update example

--- download/example/download.go
+++ download/example/download.go
@@ -3,39 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/siongui/instago"
 	"github.com/siongui/instago/download"
 	"os"
 )
 
 func main() {
-	if !igdl.IsCommandAvailable("wget") {
-		fmt.Println("Please install wget")
-		return
-	}
-	if !igdl.IsCommandAvailable("ffmpeg") {
-		fmt.Println("Please install ffmpeg")
+	mgr, err := igdl.NewInstagramDownloadManager(
+		os.Getenv("IG_DS_USER_ID"),
+		os.Getenv("IG_SESSIONID"),
+		os.Getenv("IG_CSRFTOKEN"))
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
 	typ := flag.String("downloadtype", "timeline", "Download 1) timeline 2) story 3) highlight")
 	flag.Parse()
 
-	mgr := instago.NewInstagramApiManager(
-		os.Getenv("IG_DS_USER_ID"),
-		os.Getenv("IG_SESSIONID"),
-		os.Getenv("IG_CSRFTOKEN"))
-
 	switch *typ {
 	case "timeline":
 		fmt.Println("Download timeline")
-		igdl.DownloadTimeline(mgr, 1)
+		mgr.DownloadTimeline(1)
 	case "story":
 		fmt.Println("Download Stories and Post lives")
-		igdl.DownloadStoryAndPostLive(mgr)
+		mgr.DownloadStoryAndPostLive()
 	case "highlight":
 		fmt.Println("Download all story highlights of all following users")
-		igdl.DownloadStoryHighlights(mgr)
+		mgr.DownloadStoryHighlights()
 	default:
 		fmt.Println("You have to choose a download type")
 	}
