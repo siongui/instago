39892b9a77c7615e77c2baf5012eed814c2113dc
siongui <siongui@gmail.com>
2018-05-21 23:17:22 +0800

    add func for download single user story highlights

--- download/highlight.go
+++ download/highlight.go
@@ -6,6 +6,19 @@ import (
 	"time"
 )
 
+// Download story highlights of a single user.
+func (m *IGDownloadManager) DownloadUserStoryHighlights(userid string) {
+	trays, err := m.apimgr.GetAllStoryHighlights(userid)
+	if err != nil {
+		panic(err)
+	}
+	for _, tray := range trays {
+		for _, item := range tray.GetItems() {
+			getStoryItem(item)
+		}
+	}
+}
+
 // Download story highlights of all following users
 func (m *IGDownloadManager) DownloadStoryHighlights() {
 	users, err := m.apimgr.GetFollowing(m.apimgr.GetSelfId())
@@ -17,15 +30,7 @@ func (m *IGDownloadManager) DownloadStoryHighlights() {
 	for _, user := range users {
 		fmt.Println("Fetching", user.Username, user.Pk, "stroy highlights ...")
 		userid := strconv.FormatInt(user.Pk, 10)
-		trays, err := m.apimgr.GetAllStoryHighlights(userid)
-		if err != nil {
-			panic(err)
-		}
-		for _, tray := range trays {
-			for _, item := range tray.GetItems() {
-				getStoryItem(item)
-			}
-		}
+		m.DownloadUserStoryHighlights(userid)
 		fmt.Println("Fetching", user.Username, user.Pk, "stroy highlights done!")
 
 		// sleep 3 seconds to prevent http 429
