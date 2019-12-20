b72e269df5afd0f96f56fa8e49d5f243f9f83363
siongui <tesiongui@gmail.com>
2019-12-21 01:23:06 +0800

    get all cookies in instagram.com

--- crx-cookies/popup.js
+++ crx-cookies/popup.js
@@ -4,17 +4,10 @@ chrome.tabs.query({
 }, function(tabs) {
   chrome.cookies.getAll({}, function (cookies) {
 
-    var cookieNames = ["ds_user_id", "sessionid", "csrftoken"];
-
     var cookieAuth = {};
     document.write("<pre>");
     for (var i in cookies) {
-
       var cookie = cookies[i];
-      if (cookieNames.indexOf(cookie.name) == -1) {
-        continue;
-      }
-
       cookieAuth[cookie.name] = cookie.value;
     }
     document.write(JSON.stringify(cookieAuth, null, 2));
