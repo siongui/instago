ae8b865dfaf4a862bdabbf74b64324f9dd37391a
siongui <tesiongui@gmail.com>
2020-01-25 09:32:32 +0800

    fix downloading saved posts of non-following users and post quality issue

--- download/saved.go
+++ download/saved.go
@@ -11,5 +11,13 @@ func (m *IGDownloadManager) DownloadSavedPosts(numOfItem int) {
 		log.Println(err)
 		return
 	}
-	getTimelineItems(items)
+
+	// The following code is not good because
+	// 1. Cannot download saved posts of non-following users
+	// 2. not best quality of saved posts
+	//getTimelineItems(items)
+
+	for _, item := range items {
+		m.DownloadPost(item.Code)
+	}
 }
