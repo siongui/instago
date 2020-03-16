d1bc3a39479d156438df036096a6d0eff16c5ec9
siongui <tesiongui@gmail.com>
2020-03-17 04:54:07 +0800

    add example of batch download of unexpired stories of multiple users

---
+++ download/example/userstorybatch.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/siongui/instago/download"
+)
+
+func main() {
+	mgr, err := igdl.NewInstagramDownloadManager("auth.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	usernames := []string{"google", "facebook", "instagram"}
+
+	for _, username := range usernames {
+		fmt.Println("Download unexpired stories of", username)
+		mgr.DownloadUserStoryByName(username)
+	}
+}
