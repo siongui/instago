725a1c4b356c5421b6555665476460f8a8636d61
siongui <tesiongui@gmail.com>
2020-03-31 10:39:30 +0800

    merge example

--- download/example/1dayrm.go
+++ download/example/1dayrm.go
@@ -10,37 +10,66 @@ import (
 	"time"
 )
 
+var count int = 0
+
 func isOlderThanOneDay(t time.Time) bool {
 	return time.Now().Sub(t) > 24*time.Hour
 }
 
-func processFile(path string, info os.FileInfo) {
-	filename := filepath.Base(path)
-	//if strings.Contains(filename, "-post-") || strings.Contains(filename, "-story") || strings.Contains(filename, "-postlive-") {
-	if strings.Contains(filename, "-story") || strings.Contains(filename, "-postlive-") {
-		//fmt.Println(filename)
-		tmp := strings.Split(filename, "-")
-		utimeext := tmp[len(tmp)-1]
-		utime := strings.Split(utimeext, ".")[0]
-		//fmt.Println(utime)
+func getTimeFromFilename(filename string) (t time.Time) {
+	//fmt.Println(filename)
+	tmp := strings.Split(filename, "-")
+	utimeext := tmp[len(tmp)-1]
+	utime := strings.Split(utimeext, ".")[0]
+	//fmt.Println(utime)
 
-		i, err := strconv.ParseInt(utime, 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		t := time.Unix(i, 0)
-		//fmt.Println(t.Format(time.UnixDate))
+	i, err := strconv.ParseInt(utime, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	t = time.Unix(i, 0)
+	//fmt.Println(t.Format(time.UnixDate))
+	return
+}
 
-		if isOlderThanOneDay(t) {
-			fmt.Println(filename)
-			fmt.Println(t.Format(time.UnixDate))
-			//os.Remove(path)
-		}
+func moveFile(todir, path string, info os.FileInfo) {
+	count++
+	fmt.Print(count, ": ")
+
+	//fmt.Println(info.Name())
+	//return
+
+	newpath := filepath.Join(todir, info.Name())
+	fmt.Printf("move %q to %q\n", path, newpath)
+	err := os.Rename(path, newpath)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func processFile(todir, path string, info os.FileInfo) {
+	// move files older than one day in name
+	if isOlderThanOneDay(getTimeFromFilename(info.Name())) {
+		moveFile(todir, path, info)
+		return
+	}
+
+	// move files with "-post-" in the filename
+	if strings.Contains(info.Name(), "-post-") {
+		moveFile(todir, path, info)
+		return
+	}
+
+	// move files older than one day
+	if isOlderThanOneDay(info.ModTime()) {
+		moveFile(todir, path, info)
+		return
 	}
 }
 
 func main() {
 	root := flag.String("root", "Instagram", "dir of downloaded files")
+	todir := flag.String("todir", "~/", "dir to which files are moved")
 	flag.Parse()
 
 	err := filepath.Walk(*root, func(path string, info os.FileInfo, err error) error {
@@ -49,7 +78,7 @@ func main() {
 			return err
 		}
 		if info.Mode().IsRegular() {
-			processFile(path, info)
+			processFile(*todir, path, info)
 		}
 
 		return nil
