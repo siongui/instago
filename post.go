5d6114c177efdb0a9e8ecb5e2540c603b872647b
siongui <siongui@gmail.com>
2020-09-21 03:45:32 +0800

    better code structure

--- post.go
+++ post.go
@@ -4,10 +4,7 @@ package instago
 
 import (
 	"encoding/json"
-	"strings"
 )
-
-const urlPost = `https://www.instagram.com/p/{{CODE}}/?__a=1`
 
 // Decode JSON data returned by Instagram post API
 type postInfo struct {
@@ -18,12 +15,12 @@ type postInfo struct {
 
 // Given the code of the post, return url of the post.
 func CodeToUrl(code string) string {
-	return strings.Replace(`https://www.instagram.com/p/{{CODE}}/`, "{{CODE}}", code, 1)
+	return "https://www.instagram.com/p/" + code + "/"
 }
 
 // Given code of post, return information of the post with login status.
 func (m *IGApiManager) GetPostInfo(code string) (em IGMedia, err error) {
-	url := strings.Replace(urlPost, "{{CODE}}", code, 1)
+	url := CodeToUrl(code) + "?__a=1"
 	b, err := m.getHTTPResponse(url, "GET")
 	if err != nil {
 		return
@@ -45,7 +42,7 @@ func (m *IGApiManager) GetPostInfo(code string) (em IGMedia, err error) {
 
 // Given code of post, return information of the post without login status.
 func GetPostInfoNoLogin(code string) (em IGMedia, err error) {
-	url := strings.Replace(urlPost, "{{CODE}}", code, 1)
+	url := CodeToUrl(code) + "?__a=1"
 	b, err := getHTTPResponseNoLogin(url)
 	if err != nil {
 		return
