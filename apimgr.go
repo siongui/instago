b13333d96d794b700a412440c513f1a3540eb0bc
siongui <siongui@gmail.com>
2020-09-23 21:19:01 +0800

    better code structure

--- apimgr.go
+++ apimgr.go
@@ -10,6 +10,16 @@ import (
 
 type IGApiManager struct {
 	cookies map[string]string
+	headers map[string]string
+}
+
+// Google Search: useragent ios instagram 134.00
+// https://developers.whatismybrowser.com/useragents/parse/31984417-instagram-ios-iphone-11-pro-max-webkit
+var appUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 134.0.0.25.116 (iPhone12,5; iOS 13_3_1; en_US; en-US; scale=3.00; 1242x2688; 204771128)"
+
+// SetUserAgent let you set User-Agent header in HTTP requests.
+func SetUserAgent(s string) {
+	appUserAgent = s
 }
 
 // NewInstagramApiManager returns a API manager of a logged-in Instagram user,
@@ -18,18 +28,34 @@ type IGApiManager struct {
 // The cookies, such as *ds_user_id*, *sessionid*, or *csrftoken* can be viewed
 // in Chrome Developer Tools. See https://stackoverflow.com/a/44773079
 //
-// You can get the JSON file of cookies using chrome extension in crx-cookies/
+// You can get the JSON format cookies using chrome extension in crx-cookies/
 // directory.
 func NewInstagramApiManager(authFilePath string) (*IGApiManager, error) {
-	var m IGApiManager
 	b, err := ioutil.ReadFile(authFilePath)
 	if err != nil {
-		return &m, err
+		return nil, err
 	}
 
-	m.cookies = make(map[string]string)
-	err = json.Unmarshal(b, &m.cookies)
-	return &m, err
+	cookies := make(map[string]string)
+	err = json.Unmarshal(b, &cookies)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := make(map[string]string)
+	headers["User-Agent"] = appUserAgent
+
+	return NewApiManager(cookies, headers), nil
+}
+
+// NewApiManager is a more low-level initialization function for API manager.
+// The original goal of this method is used to create API manager used in Chrome
+// extension.
+func NewApiManager(cookies, headers map[string]string) *IGApiManager {
+	var m IGApiManager
+	m.cookies = cookies
+	m.headers = headers
+	return &m
 }
 
 // GetSelfId returns the id of a Instagram user of the API manager.
--- httpresp.go
+++ httpresp.go
@@ -9,15 +9,6 @@ import (
 	"strconv"
 )
 
-// Google Search: useragent ios instagram 134.00
-// https://developers.whatismybrowser.com/useragents/parse/31984417-instagram-ios-iphone-11-pro-max-webkit
-var userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 134.0.0.25.116 (iPhone12,5; iOS 13_3_1; en_US; en-US; scale=3.00; 1242x2688; 204771128)"
-
-// SetUserAgent let you set User-Agent header in HTTP requests.
-func SetUserAgent(s string) {
-	userAgent = s
-}
-
 // Send HTTP request and get http response without login and with gis info. Used
 // in get all post codes without login.
 func getHTTPResponseNoLoginWithGis(url, gis string) (b []byte, err error) {
@@ -82,7 +73,9 @@ func (m *IGApiManager) getHTTPResponse(url, method string) (b []byte, err error)
 		req.AddCookie(&http.Cookie{Name: name, Value: value})
 	}
 
-	req.Header.Set("User-Agent", userAgent)
+	for name, value := range m.headers {
+		req.Header.Set(name, value)
+	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
