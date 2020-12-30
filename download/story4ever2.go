2ec4e5cb5290098ac96361125462f1b9e217c84d
siongui <siongui@gmail.com>
2020-12-30 08:36:10 +0800

    test new way of download reels tray

--- download/story4ever2.go
+++ download/story4ever2.go
@@ -55,13 +55,7 @@ func (m *IGDownloadManager) AccessReelsTrayOnce2Chan(cPublicUser, cPrivateUser c
 	return
 }
 
-// TwoAccountDownloadStoryForever downloads reels tray using two account.
-// interval1 is the interval for periodically accessing reels tray. interval2
-// is the interval for fetching user story via second account. interval3 is the
-// interval for fetching user story via main account. ignoreMuted will ignore
-// fetching stories of muted users if true. verbose will print more info if set
-// to true. If not sure, try (90, 60, 60, true, true). If http 429 happens, try
-// to use longer interval.
+// Under test.
 func (m *IGDownloadManager) TwoAccountDownloadStoryForever(interval1 int, interval2, interval3 int64, ignoreMuted, verbose bool) {
 	if !m.IsCleanAccountSet() {
 		fmt.Println("clean account not set. exit")
@@ -74,6 +68,30 @@ func (m *IGDownloadManager) TwoAccountDownloadStoryForever(interval1 int, interv
 	cPrivateUser := make(chan TrayInfo, 300)
 
 	go m.GetCleanAccountManager().TrayDownloader(cPublicUser, NewTimeLimiter(interval2), true, verbose)
+	go m.TrayDownloader2ChanPrivate(cPublicUser, cPrivateUser, NewTimeLimiter(interval3), true, verbose)
+
+	for {
+		err := m.AccessReelsTrayOnce2Chan(cPublicUser, cPrivateUser, ignoreMuted, verbose)
+		if err != nil {
+			log.Println(err)
+		}
+		PrintMsgSleep(interval1, "TwoAccountDownloadStoryForever: ")
+	}
+}
+
+// Under test.
+func (m *IGDownloadManager) TwoAccountDownloadStoryForeverSecondAccountViaStoryAPI(interval1 int, interval2, interval3 int64, ignoreMuted, verbose bool) {
+	if !m.IsCleanAccountSet() {
+		fmt.Println("clean account not set. exit")
+		return
+	}
+
+	// usually there are at most 150 trays in reels_tray.
+	// double the buffer to 300. 160 or 200 may be ok as well.
+	cPublicUser := make(chan TrayInfo, 300)
+	cPrivateUser := make(chan TrayInfo, 300)
+
+	go m.GetCleanAccountManager().TrayDownloaderViaStoryAPI(cPublicUser, NewTimeLimiter(interval2), true, verbose)
 	go m.TrayDownloader2ChanPrivate(cPublicUser, cPrivateUser, NewTimeLimiter(interval3), true, verbose)
 
 	for {
@@ -137,6 +155,73 @@ func (m *IGDownloadManager) TrayDownloader2ChanPrivate(cPublicUser, cPrivateUser
 			restInterval := 1
 			if verbose {
 				log.Println("TrayDownloader2ChanPrivate: current queue length is ", len(queue))
+			}
+			SleepSecond(restInterval)
+		}
+	}
+}
+
+func (m *IGDownloadManager) TrayDownloaderViaStoryAPI(c chan TrayInfo, tl *TimeLimiter, ignorePrivate, verbose bool) {
+	queue := []TrayInfo{}
+	for {
+		select {
+		case ti := <-c:
+			// append to queue if not exist
+			id := ti.Id
+			username := ti.Username
+			if IsTrayInfoInQueue(queue, ti) {
+				if verbose {
+					PrintUsernameIdMsg(username, id, "exist. ignore.", "len(c):", len(c), "len(queue):", len(queue))
+				}
+			} else {
+				queue = append(queue, ti)
+				if verbose {
+					PrintUsernameIdMsg(username, id, "appended.", "len(c):", len(c), "len(queue):", len(queue))
+				}
+			}
+		default:
+			if len(queue) > 0 {
+				ti := queue[0]
+				queue = queue[1:]
+
+				// wait at least *interval* seconds until next private API access (prevent http 429)
+				tl.WaitAtLeastIntervalAfterLastTime()
+				ut, err := m.GetUserStory(strconv.FormatInt(ti.Id, 10))
+				tl.SetLastTimeToNow()
+				if err != nil {
+					log.Println(err)
+					c <- ti
+				} else {
+					tray := ut.Reel
+					id := tray.User.GetUserId()
+					username := tray.User.GetUsername()
+					for _, item := range tray.GetItems() {
+						_, err := getStoryItem(item, tray.GetUsername())
+						if err != nil {
+							PrintUsernameIdMsg(username, id, err)
+							continue
+						}
+
+						if ti.Layer-1 < 1 {
+							continue
+						}
+						for _, rm := range item.ReelMentions {
+							PrintReelMentionInfo(rm)
+							if rm.User.IsPrivate && ignorePrivate {
+								continue
+							}
+							c <- setupTrayInfo(rm.User.Pk, rm.GetUsername(), ti.Layer-1, rm.User.IsPrivate)
+							if verbose {
+								PrintUsernameIdMsg(rm.GetUsername(), rm.User.Pk, "sent to channel (reel mention)")
+							}
+						}
+					}
+				}
+			}
+
+			restInterval := 1
+			if verbose {
+				log.Println("TrayDownloaderViaStoryAPI: current queue length is ", len(queue))
 			}
 			SleepSecond(restInterval)
 		}
